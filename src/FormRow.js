b73db1bd2b393dc348769814c103ea7283c98a4f
Michal Kokocinski <michal@waat.eu>
2019-05-24 17:21:00 +0200

    Prop that preserves FormRow spasing on mobile

--- src/FormRow.js
+++ src/FormRow.js
@@ -1,23 +1,34 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import withStyles from 'react-jss'
 import { breakpoint } from './themeHelpers'
 
 
-const FormRow = ({ children, classes }) =>
-  <div className={classes.formRow}>{children}</div>
+const FormRow = ({ children, mobile, classes }) =>
+  <div className={mobile ? classes.formRowMobile : classes.formRow}>
+    {children}
+  </div>
 
-export default withStyles(theme => ({
-  formRow: {
-    [breakpoint(theme.breakpoints.sm)]: {
-      display: 'flex',
-      alignItems: 'flex-end',
-      marginBottom: theme.sizes.inputGutterBottom,
-      '& .form-control': {
-        marginBottom: 0,
-      },
-      '& > div + *': {
-        marginLeft: theme.sizes.formRowGutter,
-      },
+FormRow.propTypes = {
+  mobile: PropTypes.bool,
+}
+
+export default withStyles(theme => {
+  const styling = {
+    display: 'flex',
+    alignItems: 'flex-end',
+    marginBottom: theme.sizes.inputGutterBottom,
+    '& .form-control': {
+      marginBottom: 0,
     },
-  },
-}))(FormRow)
+    '& > div + *': {
+      marginLeft: theme.sizes.formRowGutter,
+    },
+  }
+  return {
+    formRow: {
+      [breakpoint(theme.breakpoints.sm)]: styling,
+    },
+    formRowMobile: styling,
+  }
+})(FormRow)
