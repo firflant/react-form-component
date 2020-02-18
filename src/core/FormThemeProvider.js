3408c65ef61981f2da6e4e71e02ebcf041b6673a
Michal Kokocinski <michal@waat.eu>
2020-02-18 14:32:19 +0100

    Prevent unneccesary rerenders of FormThemeProvider

--- src/core/FormThemeProvider.js
+++ src/core/FormThemeProvider.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ThemeProvider } from 'react-jss'
+import { ThemeProvider, useTheme } from 'react-jss'
 import { ToastContainer } from 'react-toastify'
 import defautTheme from './theme'
 import 'react-toastify/dist/ReactToastify.css'
@@ -7,33 +7,38 @@ import 'react-toastify/dist/ReactToastify.css'
 
 const FormThemeProvider = ({ theme, children }) => {
   const [isRoot, setIsRoot] = React.useState(false)
-  const [toastContainerProps, setToastContainerProps] = React.useState()
+  const [currentTheme, setCurrentTheme] = React.useState()
+  const [toastContainerProps, setToastContainerProps] = React.useState({})
+  const outerTheme = useTheme()
+
+  React.useEffect(() => {
+    if (!outerTheme) {
+      setIsRoot(true)
+    }
+    const parentTheme = outerTheme || defautTheme
+    const parsedTheme = {
+      sizes: { ...parentTheme.sizes, ...theme.sizes },
+      colors: { ...parentTheme.colors, ...theme.colors },
+      typography: { ...parentTheme.typography, ...theme.typography },
+      breakpoints: { ...parentTheme.breakpoints, ...theme.breakpoints },
+      textLabels: { ...parentTheme.textLabels, ...theme.textLabels },
+      toastContainerProps: { ...parentTheme.toastContainerProps, ...theme.toastContainerProps },
+      ...parentTheme.customValidationFunction,
+      ...theme.customValidationFunction,
+    }
+    setCurrentTheme(parsedTheme)
+    setToastContainerProps(parsedTheme.toastContainerProps)
+  }, [])
+
   return (
-    <ThemeProvider theme={outerTheme => {
-      if (!outerTheme && !isRoot) {
-        setIsRoot(true)
-      }
-      const parentTheme = outerTheme || defautTheme
-      const currentTheme = {
-        sizes: { ...parentTheme.sizes, ...theme.sizes },
-        colors: { ...parentTheme.colors, ...theme.colors },
-        typography: { ...parentTheme.typography, ...theme.typography },
-        breakpoints: { ...parentTheme.breakpoints, ...theme.breakpoints },
-        textLabels: { ...parentTheme.textLabels, ...theme.textLabels },
-        toastContainerProps: { ...parentTheme.toastContainerProps, ...theme.toastContainerProps },
-        ...parentTheme.customValidationFunction,
-        ...theme.customValidationFunction,
-      }
-      if (!toastContainerProps) {
-        setToastContainerProps(currentTheme.toastContainerProps)
-      }
-      return currentTheme
-    }}>
-      <React.Fragment>
-        {children}
-        {isRoot && toastContainerProps && <ToastContainer {...toastContainerProps} />}
-      </React.Fragment>
-    </ThemeProvider>
+    currentTheme
+      ? <ThemeProvider theme={currentTheme}>
+        <React.Fragment>
+          {children}
+          {isRoot && toastContainerProps && <ToastContainer {...toastContainerProps} />}
+        </React.Fragment>
+      </ThemeProvider>
+      : null
   )
 }
 
