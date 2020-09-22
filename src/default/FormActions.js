e5909eb3741ab78d7edad7bde01b7944d33a2826
Michal Kokocinski <michal@waat.eu>
2020-09-22 15:39:00 +0200

    Add align prop to FormActions component.

--- src/default/FormActions.js
+++ src/default/FormActions.js
@@ -1,18 +1,30 @@
 import React from 'react'
 import withStyles from 'react-jss'
+import PropTypes from 'prop-types'
 
 
-const FormActions = ({ children, classes }) =>
-  <div className={classes.formActions}>{children}</div>
+const FormActions = ({ children, classes, align }) =>
+  <div
+    className={classes.formActions}
+    style={{ justifyContent: align === 'left'
+      ? 'flex-start'
+      : align === 'right'
+        ? 'flex-end'
+        : 'center',
+    }}
+  >{children}</div>
+
+FormActions.propTypes = {
+  align: PropTypes.oneOf(['left', 'right']),
+}
 
 export default withStyles(theme => ({
   formActions: {
     marginTop: theme.sizes.formActionsDistance,
     lineHeight: 'normal',
     display: 'flex',
-    justifyContent: 'flex-end',
-    flexWrap: 'wrap',
     alignItems: 'center',
+    flexWrap: 'wrap',
     margin: -10,
     '& > *': {
       margin: 10,
